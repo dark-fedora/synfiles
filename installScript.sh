5fb89a07f3c9b917a1e72296ddd50e448ca01866
dark-fedora <f3d0r4.047@gmail.com>
2021-05-28 11:20:50 -0700

    maybe now

--- installScript.sh
+++ installScript.sh
@@ -1,6 +1,6 @@
 mkdir -p ~/.vim/before/syntax
-curl -sL "https://raw.githubusercontent.com/dark-fedora/synfiles/master/before/syntax/javascript.vim"
+curl -sL https://raw.githubusercontent.com/dark-fedora/synfiles/master/before/syntax/javascript.vim > ~/.vim/before/syntax
 mkdir -p ~/.vim/after/syntax
-curl -sL "https://raw.githubusercontent.com/dark-fedora/synfiles/master/after/syntax/javascript.vim"
+curl -sL https://raw.githubusercontent.com/dark-fedora/synfiles/master/after/syntax/javascript.vim > ~/.vim/after/syntax
 mkdir -p ~/.vim/syntax
-curl -sL "https://raw.githubusercontent.com/dark-fedora/synfiles/master/syntax/javascript.vim"
+curl -sL https://raw.githubusercontent.com/dark-fedora/synfiles/master/syntax/javascript.vim > ~/.vim/syntax
